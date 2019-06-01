ad294933f193c8ef4e05ce647d7251a5afe1b3dc
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-06-01 02:09:25 +0100

    Adding Dummy Data

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,14 +2,18 @@ import React, { Component } from "react";
 import Notification from "./Notifications";
 
 import ProjectList from "../projects/ProjectList";
+
+
+import {connect} from 'react-redux';
 class Dashboard extends Component {
   state = {};
   render() {
+    const {projects}  = this.props;
     return (
       <div className="dashboard container">
       <div className="row">
       <div className="col s12 m6">
-          <ProjectList />
+          <ProjectList projects={projects} />
         </div>
         <div className="col s12 m5 offset-m1">
           <Notification />
@@ -21,4 +25,9 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+const mapStateToProps = (state) =>{
+  return{
+    projects : state.project.projects
+  }
+}
+export default connect(mapStateToProps)(Dashboard);
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 
-const ProjectSummary =  () =>{
+const ProjectSummary =  ({project}) =>{
 
     return(
         <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted By The Net Ninja</p>
+          <span className="card-title">{project.title}</span>
+          <p>{project.content}</p>
           <p className="grey-text">3rd september</p>
         </div>
       </div>

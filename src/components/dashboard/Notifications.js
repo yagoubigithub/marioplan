15d4b0976b76832df6ba2ebb9598bcb6162971ed
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-06-03 06:30:07 +0100

    Notification component

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Notification = () => {
-
-    return(<div>
-        <p>Notification</p>
-    </div>)
-}
+  return (
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Notifications</span>
+            <ul className="notifications">
+              <li>Notification</li>
+              <li>Notification</li>
+              <li>Notification</li>
+              <li>Notification</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+   
+  );
+};
 
 export default Notification;
